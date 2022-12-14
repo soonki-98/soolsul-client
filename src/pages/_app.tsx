f88e792cce1cbe085202e5dca75d82b16854e3e2
soonki-98 <alstnsrl98@gmail.com>
2022-12-14 23:40:37 +0900

    [Feat] react-query 적용 #41

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,23 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import { useMemo } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import useResize from '@hooks/common/useResize';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = useMemo(() => new QueryClient(), []);
+
   useResize();
 
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
